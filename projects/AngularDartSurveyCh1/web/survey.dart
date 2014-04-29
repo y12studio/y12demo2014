c943a1e3ee276e507e10a927c72d1cdf60640e2b
y12studio <y12studio@gmail.com>
2014-04-29 16:53:38 +0800

    AngularDart Survey CH1 ref #4
    only bind one input

---
+++ projects/AngularDartSurveyCh1/web/survey.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2014, y12studio.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// Apache license.
+import 'package:angular/application_factory.dart';
+
+void main() {
+  applicationFactory().run();
+}
+
